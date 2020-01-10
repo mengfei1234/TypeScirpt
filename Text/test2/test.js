b1ddbcdc7fd63f3ee83dd1c09ab3eca5b80ee8b5
mengfei1234 <lmf20151234@163.com>
2020-01-10 15:47:41 +0800

    基础语法、基础类型、变量声明部分提交

---
+++ Text/test2/test.js
@@ -0,0 +1,11 @@
+var sname = 'Tom';
+var score1 = 20;
+var score2 = 1;
+var score = score1 + score2;
+console.log(sname);
+console.log(score);
+
+
+
+
+
---
+++ Text/test2/test.ts
@@ -0,0 +1,6 @@
+var sname:string='Tom'
+var score1:number=20;
+var score2:number=1;
+var score=score1+score2;
+console.log(sname)
+console.log(score)
